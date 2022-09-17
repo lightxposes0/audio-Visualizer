275bfb53b17fd900cbc9ba3a4097b089eb804a9d
Stian <stian.larsen@mac.com>
2022-09-17 02:48:32 +0200

    Created a start for my home.js, the file has now successfully listed my database objects, and the files are showing up. just need to get on with the css styling (scss)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,40 +11,44 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
-    const songsCollectionReference = collection(db, "songs");
+  //  const songsCollectionReference = collection(db, "songs");
     ///// test
-    useEffect(  ()  => {
+    // useEffect(  ()  => {
 
-        // Create the funtion that grab´s the data from our database, logs them to console, and pushes the data into our useState. 
-        const getSongs = async () => {
-        const data = await getDocs(songsCollectionReference);
-        // console.log("Data retrieved from the firebase firestore database:" + data)
-            setSongs(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
-        };   
-        // On page load, our function will run, and our data will sync asap on our website. 
+    //     // Create the funtion that grab´s the data from our database, logs them to console, and pushes the data into our useState. 
+    //     const getSongs = async () => {
+    //     const data = await getDocs(songsCollectionReference);
+    //     // console.log("Data retrieved from the firebase firestore database:" + data)
+    //         setSongs(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    //     };   
+    //     // On page load, our function will run, and our data will sync asap on our website. 
         
         
-        getSongs()
+    //     getSongs()
         
-        }, []);
+    //     }, []);
         
 
 
     ////// test
 
-/*
-    useEffect(() => {
-        setLoading(true)
-        const retrieveSongs = onSnapshot(collection(db, "songs"), (snapshot) => {
-            let arrayList = [];
-            snapshot.docs.forEach((doc) => {
-                arrayList.push({id: doc.id, ...doc.data})
-            });
-            setSongs(arrayList);
-            setLoading(false);
 
-        }, (error) => {console.log(error);
-        }
+    useEffect(() => {
+        setLoading(true);
+        const retrieveSongs = onSnapshot(
+            collection(db, "songs"), 
+            (snapshot) => {
+                let arrayList = [];
+                snapshot.docs.forEach((doc) => {
+                    arrayList.push({ id: doc.id, ...doc.data() });
+                });
+                setSongs(arrayList);
+                setLoading(false);
+
+            }, 
+            (error) => {
+                console.log(error);
+            }
         );
 
         return () => {
@@ -52,7 +56,7 @@ const Home = () => {
         };
         
     }, []);
-    */
+    
     return (
         <div className='home_wrapper'>
             <>
