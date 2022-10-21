7cf0e1eef61dd8ca191169ca930ade38817fa25a
Stian <stian.larsen@mac.com>
2022-10-21 12:18:25 +0200

    Last commit for now

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyCEyWS1yR_HNT9c9Ujaxu94mde0cImP2jM",
+    authDomain: "audiovisualizer-35bdf.firebaseapp.com",
+    projectId: "audiovisualizer-35bdf",
+    storageBucket: "audiovisualizer-35bdf.appspot.com",
+    messagingSenderId: "360955257040",
+    appId: "1:360955257040:web:3f319ab5b523b3ddc27ed5",
+    measurementId: "G-MN84MHSXPH"
+}
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import {    getStorage, storage } from "firebase/storage"
+const config = require("./config")
 
 const firebaseConfig = {
-apiKey: "AIzaSyCEyWS1yR_HNT9c9Ujaxu94mde0cImP2jM",
-    authDomain: "audiovisualizer-35bdf.firebaseapp.com",
-    projectId: "audiovisualizer-35bdf",
-    storageBucket: "audiovisualizer-35bdf.appspot.com",
-    messagingSenderId: "360955257040",
-    appId: "1:360955257040:web:3f319ab5b523b3ddc27ed5",
-    measurementId: "G-MN84MHSXPH"
+apiKey: config.apiKey,
+    authDomain: config.authDomain,
+    projectId: config.projectId,
+    storageBucket: config.storageBucket,
+    messagingSenderId: config.messagingSenderId,
+    appId: config.appId,
+    measurementId: config.measurementId
 };
 
 
